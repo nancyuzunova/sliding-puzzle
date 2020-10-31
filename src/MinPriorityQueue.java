98081a1ba1d950a82e5195f4beb3208ea3969b97
Nancy Uzunova <nansy_98@abv.bg>
2020-10-31 20:50:20 +0200

    Implemented own MinProprityQueue which orders the Search Nodes in it with the minimum priority first.

--- src/MinPriorityQueue.java
+++ src/MinPriorityQueue.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
-public class MinPriorityQueue {
+public class MinPriorityQueue{
+
+    private List<SearchNode> elements;
+
+    public MinPriorityQueue() {
+        this.elements = new ArrayList<>();
+    }
+
+    public void offer(SearchNode element){
+        for (int i = 0; i < elements.size(); i++) {
+            if (element.getPriority() < elements.get(i).getPriority()){
+                elements.add(i, element);
+                return;
+            }
+        }
+        elements.add(element);
+    }
+
+    public SearchNode poll(){
+        return elements.remove(0);
+    }
+
+    public boolean isEmpty(){
+        return elements.isEmpty();
+    }
+
+    public int size(){
+        return elements.size();
+    }
 }
